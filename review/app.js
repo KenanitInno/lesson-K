9a2f89859ffaf32cc723ae3b63dd5f5f7c1cbbca
KenanitInno <157836594+KenanitInno@users.noreply.github.com>
2024-04-14 11:21:38 +0400

    Add files via upload

--- review/app.js
+++ review/app.js
@@ -2,3 +2,68 @@ Input: num1 = 5, num2 = 3
 Output: 8
 Input: num1 = 13, num2 = 6
 Output: 19
+
+num1 = 10
+num2 = 5
+result = num1 - num2
+print("Subtraction using the Minus Operator:", result)
+
+a = 4 , b = 10
+a * b === 4 * 10 === 40
+
+a =  10 
+b = 5 
+a / b === 10 / 5 === 2 
+
+Input: 2
+Output:  number
+
+Input: 41
+Output:  Number
+
+Input:  n = 11
+Output: True
+Input:  n = 1
+Output: False
+
+Input: a = 2, b = 4
+Output: 4
+
+Input: a = -1, b = -4
+Output: -1
+
+Input: a = 2, b = 4
+Output: 2
+
+Input: a = -1, b = -4
+Output: -4
+
+0 <= arr.length <= 1000
+-10^6 <= arr[i] <= 10^6
+
+ my_function(x)
+  return x[-1]
+
+mytxt = my_function("I wonder how this text looks like backwards")
+
+print(mytxt)
+
+
+
+ isPalindrome(s)
+	return s == s[-1]
+
+
+
+s = "malayalam"
+ans = isPalindrome(s)
+
+let myString = "HeLLo WoRLD!";
+let lowerCaseString = myString.toLowerCase();
+
+const str = 'coding beauty';
+const noVowels = str.replace(/[aeiou]/gi, '');
+console.log(noVowels); // cdng bty
+
+let regex = /^[a-zA-Z]+$/;
+
